d340199ebcdaad2ef798990d24c6fbce57b85e46
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-10-09 14:37:17 +0300

    add login endpoint

--- AuthenticationAPI/Controllers/UserController.cs
+++ AuthenticationAPI/Controllers/UserController.cs
@@ -41,6 +41,21 @@ namespace AuthenticationAPI.Controllers
             return Ok("User successfull created!");
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email.Equals(request.Email));
+
+            if (user is null) return BadRequest("User not found");
+
+            if (user.VerifiedAt is null) return BadRequest("Not verified");
+
+            if (!PasswordHelper.VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+                return BadRequest("Password is incorrect...");
+
+            return Ok($"Welcome back, {user.Email}!😊");
+        }
+
         
     }
 }
--- AuthenticationAPI/Utils/PasswordHelper.cs
+++ AuthenticationAPI/Utils/PasswordHelper.cs
@@ -13,5 +13,15 @@ namespace AuthenticationAPI.Utils
                 passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
             }
         }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
     }
 }
