77e1e4db4fadf25d2d04fc9eff9baa732b0a23f4
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-10-09 14:28:51 +0300

    add user login request

---
+++ AuthenticationAPI/Models/UserLoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationAPI.Models
+{
+    public class UserLoginModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
