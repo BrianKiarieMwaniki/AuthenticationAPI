ff670d58c7a100a7a8d24247e4650fa9738350e1
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-10-09 15:24:39 +0300

    add auto mapper

---
+++ AuthenticationAPI/Profiles/UsersProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace AuthenticationAPI.Profiles
+{
+    public class UsersProfiles : Profile
+    {
+        public UsersProfiles()
+        {
+            CreateMap<RegisterDTO, User>();
+            CreateMap<LoginDTO, User>();
+            CreateMap<ResetPasswordDTO, User>();
+        }
+    }
+}
