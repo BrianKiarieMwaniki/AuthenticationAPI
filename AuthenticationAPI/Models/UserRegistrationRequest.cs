6e1a024b31f24c96eb1a9274a0f796ed652a173c
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-10-09 14:08:00 +0300

    add user registration model

---
+++ AuthenticationAPI/Models/UserRegistrationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationAPI.Models
+{
+    public class UserRegistrationRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
